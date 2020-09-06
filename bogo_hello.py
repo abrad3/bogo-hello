49aacdc45448e95b7cf48be80afa143c4451f57a
work-goose <tiggyb3@gmail.com>
2020-09-06 15:38:51 +1000

    added beautiful soup bit

--- bogo_hello.py
+++ bogo_hello.py
@@ -1,12 +1,28 @@
-from bs4 import BeautifulSoup
-import random
+mysterious_numbers=[2535,853,288,2535,154,3767,154,2725,495,2535,855,2568]
 
+from bs4 import BeautifulSoup
+from urllib import request
+url = "http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html"
+sauce = request.urlopen(url).read().decode('utf8')
+
+soup = BeautifulSoup(sauce, 'html.parser')
+tasty_gloop = soup.body.text
+start_pi = 24
+end_pi = 101307
+raw_pie = tasty_gloop[start_pi:end_pi]
+cooked_pie= "".join(tasty for tasty in raw_pie if tasty not in '\n')
+
+pie_triplets=[]
+for wow in mysterious_numbers:
+    pie_triplets.append(int(cooked_pie[wow:wow+3]))
+
+message=list(map(chr, pie_triplets))
+
+import random
 def bogo_hello(ls):
     random.shuffle(ls)
     return
 
-
-string = list("leHlo! owrld")
-while string != list("Hello world!"):
-    bogo_hello(string)
-    print(''.join(string))
+while message != list("Hello world!"):
+   bogo_hello(message)
+   print(''.join(message))
